e889a1af31f06f9d1a32e675d83f6d20bc483c76
sherry <sherry.s.ruan@gmail.com>
2017-10-03 14:28:09 -0700

    changed model

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     try: 
-                        if (user_answer_time is False):
+                        if (user_answer_time == False):
                             question, question_id = tfidf.pickRandomQuestion()
                             send_message(sender_id, question)
                             user_answer_time = True
