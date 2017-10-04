ab9d65154da5d0069102eb350f36e8a90635c804
sherry <sherry.s.ruan@gmail.com>
2017-10-03 20:49:42 -0700

    changed model

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     print("sender ID is: "+sender_id)
                     print("recipient ID is: "+recipient_id)
-                    #print question_id.keys()
+                    print("\n-1- QID is: "+QID+"\n")
 
                     if sender_id == "1497174250389598": #chatbot
                         return "irrelavant ID", 200
@@ -67,7 +67,7 @@ def webhook():
                         question, QID = tfidf.pickRandomQuestion()
                         send_message(sender_id, "Question."+str(QID)+": "+question)
                     else:
-                        print("question ID is:"+str(QID))
+                         print("\n-2- QID is: "+QID+"\n")
                         print("true"+"="*50)
                         standard_answer, score = tfidf.computeScore(message_text, QID)
                         send_message(sender_id, "Answer." +str(QID) + ": "+standard_answer)
@@ -75,7 +75,7 @@ def webhook():
 
                         question, QID = tfidf.pickRandomQuestion()
                         send_message(sender_id, "Question."+str(QID)+": "+question)
-                        print("question ID is:"+str(QID))
+                         print("\n-3- QID is: "+QID+"\n")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
