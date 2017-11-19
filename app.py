b1eed2f393a5e7ce2325ed4c814cb04d0da4d509
Zhengneng <qiuzhengneng@gmail.com>
2017-11-19 12:47:57 -0800

    fixed print statement .2

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def webhook():
                             QID = app.session[sender_id]["QID"]
 
                             print ("\napp.session_sender_id_answering: {0} \n".format(app.session[sender_id]["answering"]))
-                            print ("\nmessage text: " + message_text + "\n")
+                            print ("\nmessage text: " + message_text[:-9] + "\n")
 
                             if message_text == "Switch Subject" :
                                 app.session[sender_id]["answering"] = False
