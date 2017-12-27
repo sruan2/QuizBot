01ac08a3edeb761982eb4cc5c6b18e6e655008c8
Zhengneng <qiuzhengneng@gmail.com>
2017-12-27 13:14:00 -0800

    test 4

--- app.py
+++ app.py
@@ -525,7 +525,7 @@ def show_top_10():
 
     cur = con.cursor()
     cur.execute("select t2.user_firstname,t2.user_lastname,t1.sc from \
-        (select user_id, sum(score) as sc from scores group by user_id order by sc desc limit 10) t1) join users t2 on t2.user_id = t1.user_id")
+        (select user_id, sum(score) as sc from scores group by user_id order by sc desc limit 10) t1 join users t2 on t2.user_id = t1.user_id")
 
     rows = cur.fetchall();
     return rows
