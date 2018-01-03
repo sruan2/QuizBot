2f635e62984ba942b2d1db7408f844812c3023e0
Zhengneng <qiuzhengneng@gmail.com>
2018-01-02 20:42:56 -0800

    reverted back to the tfidf score calculation

--- tfidf.py
+++ tfidf.py
@@ -58,15 +58,15 @@ class tfidfTransform():
     def getAnswer(self, QID):
         return self.QA_KB.AKB[QID].rstrip()
 
-    # def computeScore(self, user_answer, QID):
-    #     user_answer = user_answer.lower()
-    #     picked_answer = self.AKB[QID].rstrip()
-    #     answer = [picked_answer]
-    #     answer.append(user_answer)
-    #     self.tfidf_features = TfidfVectorizer().fit_transform(answer)
-    #     cosine_similarities = linear_kernel(self.tfidf_features[0:1], self.tfidf_features).flatten()
-    #     print("Similarity between the standard answer and yours is: " + str(int(cosine_similarities[1]*10)))
-    #     return picked_answer, int(cosine_similarities[1]*10)
+    def computeScore(self, user_answer, QID):
+        user_answer = user_answer.lower()
+        picked_answer = self.AKB[QID].rstrip()
+        answer = [picked_answer]
+        answer.append(user_answer)
+        self.tfidf_features = TfidfVectorizer().fit_transform(answer)
+        cosine_similarities = linear_kernel(self.tfidf_features[0:1], self.tfidf_features).flatten()
+        print("Similarity between the standard answer and yours is: " + str(int(cosine_similarities[1]*10)))
+        return picked_answer, int(cosine_similarities[1]*10)
 
     # def get_support(self, QID):
     #     return self.SKB[QID]    
