7560520bf3595dc1a5af31e9ba0f699d19b17596
Zhengneng <qiuzhengneng@gmail.com>
2018-01-09 22:45:36 -0800

    fixed get answer 3

--- QAKnowledgebase.py
+++ QAKnowledgebase.py
@@ -35,7 +35,7 @@ class QATransform():
                     self.SubKB[line.rstrip()] = [i]
                 i += 1
         f.close()
-        print (self.SubKB.keys())
+        print (self.SubKB['physics'])
         print("="*87+"\n"+"Subject KB is appended. Total Subject Count is: "+str(len(self.SubKB)))
 
     def appendSupportKB(self, SupportFile):
