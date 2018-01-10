9b3972dd67dae761ecaa2b0c96a349411d50297a
Zhengneng <qiuzhengneng@gmail.com>
2018-01-09 22:45:36 -0800

    fixed get answer 3

--- QAModel.py
+++ QAModel.py
@@ -40,7 +40,7 @@ class QAModel(object):
         return picked_question
 
     def getAnswer(self, QID):
-        print ("%"*50 + str(QID))
+        print ("%"*100 + str(QID))
         return self.QA_KB.AKB[QID].rstrip()
     
     @abstractmethod
