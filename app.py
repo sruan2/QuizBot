028e087636256aa3d4a188266d00ce5766007cf2
Zhengneng <qiuzhengneng@gmail.com>
2018-01-09 22:35:00 -0800

    fixed random 2

--- app.py
+++ app.py
@@ -301,7 +301,7 @@ def webhook():
                                 send_message(sender_id, "Question."+str(QID)+": "+question)
                             
                             elif message_text == "Random":
-                                question, QID = qa_md.pickRandomQuestion(sender_id)
+                                question, QID = qa_md.pickRandomQuestion()
                                 update_status(sender_id, 0)
                                 time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
                                 insert_question(sender_id,QID,message_text.lower(),time)
