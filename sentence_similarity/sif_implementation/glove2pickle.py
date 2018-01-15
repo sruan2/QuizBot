3430367e15e54724cda240a5fce0a1f9fb0a23cf
Zhengneng <qiuzhengneng@gmail.com>
2018-01-14 18:59:28 -0800

    updatd glove2pickle

--- sentence_similarity/sif_implementation/glove2pickle.py
+++ sentence_similarity/sif_implementation/glove2pickle.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-glove = pd.read_csv('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B.300d.txt', sep=" ", quoting=3, header=None, index_col=0)
-glove2 = {key: val.values for key, val in glove.T.items()}
+# glove = pd.read_csv('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B.300d.txt', sep=" ", quoting=3, header=None, index_col=0)
+# glove2 = {key: val.values for key, val in glove.T.items()}
 
-import pickle
-with open('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B/glove.6B.300d.pkl', 'wb') as output:
-    pickle.dump(glove2, output)
-output.close()
+# import pickle
+# with open('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B/glove.6B.300d.pkl', 'wb') as output:
+#     pickle.dump(glove2, output)
+# output.close()
 
 
 glove = pd.read_csv('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B.100d.txt', sep=" ", quoting=3, header=None, index_col=0)
@@ -18,19 +18,19 @@ with open('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B/glove.6B.100d
 output.close()
 
 
-glove = pd.read_csv('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B.200d.txt', sep=" ", quoting=3, header=None, index_col=0)
-glove2 = {key: val.values for key, val in glove.T.items()}
+# glove = pd.read_csv('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B.200d.txt', sep=" ", quoting=3, header=None, index_col=0)
+# glove2 = {key: val.values for key, val in glove.T.items()}
 
-import pickle
-with open('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B/glove.6B.200d.pkl', 'wb') as output:
-    pickle.dump(glove2, output)
-output.close()
+# import pickle
+# with open('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B/glove.6B.200d.pkl', 'wb') as output:
+#     pickle.dump(glove2, output)
+# output.close()
 
 
-glove = pd.read_csv('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B.50d.txt', sep=" ", quoting=3, header=None, index_col=0)
-glove2 = {key: val.values for key, val in glove.T.items()}
+# glove = pd.read_csv('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B.50d.txt', sep=" ", quoting=3, header=None, index_col=0)
+# glove2 = {key: val.values for key, val in glove.T.items()}
 
-import pickle
-with open('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B/glove.6B.50d.pkl', 'wb') as output:
-    pickle.dump(glove2, output)
-output.close()
+# import pickle
+# with open('/home/ubuntu/QuizBot/sentence_similarity/glove/glove.6B/glove.6B.50d.pkl', 'wb') as output:
+#     pickle.dump(glove2, output)
+# output.close()
