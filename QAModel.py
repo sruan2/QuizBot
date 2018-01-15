a0467e58c2975ace2c8cf653c0a8bcfafe226dc0
Zhengneng <qiuzhengneng@gmail.com>
2018-01-14 19:10:06 -0800

    import wordembeddings

--- QAModel.py
+++ QAModel.py
@@ -4,6 +4,7 @@ from sentence_similarity.princeton_sif import sif_sentence_similarity
 from sentence_similarity.sif_implementation import model
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+from sentence_similarity.sif_implementation.wordembeddings import EmbeddingVectorizer
 import random
 from random import randint
 import os
