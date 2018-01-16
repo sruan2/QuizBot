1b52fcffa2c9a74fd9d74d6707b0c8b3b1087b8b
Zhengneng <qiuzhengneng@gmail.com>
2018-01-15 17:50:36 -0800

    fixed cosine_similarity

--- sentence_similarity/sif_implementation/model.py
+++ sentence_similarity/sif_implementation/model.py
@@ -9,7 +9,8 @@ def preprocess(sentences, tokenizer):
     return [tokenizer.tokenize(q.lower()) for q in sentences]
 
 def cosine_similarity(v1, v2):
-    return int(((np.dot(v1, v2.T) / (np.linalg.norm(v1) * np.linalg.norm(v2)))[0,0]+1)*5) # shape is (0,10)
+    #return int(((np.dot(v1, v2.T) / (np.linalg.norm(v1) * np.linalg.norm(v2)))[0,0]+1)*5) # shape is (0,10)
+    return int(((np.dot(v1, v2.T) / (np.linalg.norm(v1) * np.linalg.norm(v2)))+1)*5) # shape is (0,10)
 
 if __name__ == '__main__': # for testing
     sentences = ['this is an example sentence', 
@@ -48,4 +49,5 @@ if __name__ == '__main__': # for testing
     V_query = emb.transform(tokenized_query)
 
     print("similarity: " + str(cosine_similarity(V_query[0], V[0]))+ "\n")
+
 
