c82e4ffdfbc548b9244e7b449f9c100d2a4a360c
Zhengneng <qiuzhengneng@gmail.com>
2018-01-15 16:31:12 -0800

    updated sif2 self.V to 0

--- sentence_similarity/sif_implementation/model.py
+++ sentence_similarity/sif_implementation/model.py
@@ -9,7 +9,7 @@ def preprocess(sentences, tokenizer):
     return [tokenizer.tokenize(q.lower()) for q in sentences]
 
 def cosine_similarity(v1, v2):
-    return int(((np.dot(v1, v2.T) / (np.linalg.norm(v1) * np.linalg.norm(v2)))[0,0]+1)*5) # shape is (0,0)
+    return int(((np.dot(v1, v2.T) / (np.linalg.norm(v1) * np.linalg.norm(v2)))[0,0]+1)*5) # shape is (0,10)
 
 if __name__ == '__main__': # for testing
     sentences = ['this is an example sentence', 
