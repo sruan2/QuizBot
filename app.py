62c76ea7deb49febdb82e549d26df206dadf84fa
Zhengneng <qiuzhengneng@gmail.com>
2018-01-20 20:21:26 -0800

    test mysql configuration

--- app.py
+++ app.py
@@ -33,10 +33,11 @@ from flask import Flask, request
 
 app = Flask(__name__)
 mysql = MySQL(app)
-app.config['MYSQL_DATABASE_USER'] = 'ubuntu'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'smartprimer'
-app.config['MYSQL_DATABASE_DB'] = 'QUIZBOT'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'ubuntu'
+app.config['MYSQL_PASSWORD'] = 'smartprimer'
+app.config['MYSQL_DB'] = 'QUIZBOT'
 mysql.init_app(app)
 
 @app.route('/test', methods=['GET'])
