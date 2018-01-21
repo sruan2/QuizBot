506774a972281cafdd2c460cc2dd8b5542d81c1c
Zhengneng <qiuzhengneng@gmail.com>
2018-01-20 22:00:05 -0800

    updated mysql query 7

--- app.py
+++ app.py
@@ -687,8 +687,9 @@ def insert_user(user_id,user_firstname,user_lastname,user_gender,user_status):
         # finally:
         #     con.close()  
         con = mysql.connection
-        cur = con.cursor()           
-        cur.execute('''INSERT INTO users (user_id,user_firstname,user_lastname,user_gender,user_status) VALUES ({0},{1},{2},{3},{4})'''.format(user_id,user_firstname,user_lastname,user_gender,user_status))           
+        cur = con.cursor()    
+        print ()       
+        cur.execute("INSERT INTO users (user_id,user_firstname,user_lastname,user_gender,user_status) VALUES (%s, %s, %s, %s, %s)",(user_id,user_firstname,user_lastname,user_gender,user_status))           
         con.commit()  
 
 # update user question-answer loop status
