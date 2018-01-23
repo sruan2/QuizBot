8098c3bb820e70f152be21a796b2ac9f55c30f5f
sherry <sherry.s.ruan@gmail.com>
2018-01-22 22:55:47 -0800

    debug sif2

--- QAModel.py
+++ QAModel.py
@@ -13,7 +13,7 @@ from random import randint
 import os
 import pickle
 from nltk import RegexpTokenizer
-import math
+
 
 
 class QAModel(object):
@@ -125,7 +125,7 @@ class SIF2Model(QAModel):
         self.V = self.emb.fit_transform(self.tokenized_sentences) # for QuizBot replace tokenized_sentences with the entire KB answers
         with open("log", "a+") as f:
             for idx, v in enumerate(self.V):
-                if math.isnan(v):
+                if v != v:
                     f.write("[NAN] " + str(idx) +" " + akb[idx])
         
         
