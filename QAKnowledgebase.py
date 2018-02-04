5fd983d3aadd1455ed0064a0d829fef03bb6826d
sherry <sherry.s.ruan@gmail.com>
2018-02-03 21:14:57 -0800

    make the code work with new json data

--- QAKnowledgebase.py
+++ QAKnowledgebase.py
@@ -4,7 +4,7 @@ import json
 class ConstructQA():
 
     def __init__(self, jsonFile):
-        print("\n" + str(os.getpid()) + ": Begin to construct QA knowledgebase\n")
+        print("[QUIZBOT] "+str(os.getpid()) + ": Begin to Construct QA Knowledgebase\n")
         data = json.load(open('../SciQdataset-23/test.json'))
 
         self.QKB = [] # question
@@ -26,50 +26,16 @@ class ConstructQA():
             self.D3KB.append(entry["distractor3"])
             self.SubKB.append(entry["subject"])
             self.DifficultyKB.append(entry["difficulty"])
-
-
-        self.appendQuestionKB(QuestionFile)
-        self.appendSupportKB(SupportFile)
-        self.appendCorrectAnswerKB(CorrectAnswerFile)
-        self.appendSubjectKB(SubjectFile)
-
-        assert(len(self.QKB) == len(self.SKB))
-        assert(len(self.QKB) == len(self.AKB))
+        self.appendSubDict()
         
-        print("\n" + str(os.getpid()) + "Finished QA knowledgebase Construction\n")
+        print("[QUIZBOT] " + str(os.getpid()) + ": Finished QA Knowledgebase Construction\n")
     
-    def appendQuestionKB(self, QuestionFile):
-        with open(QuestionFile, 'r') as f:
-            print("="*87+"\n"+"File opened: "+QuestionFile)
-            for line in f:
-                self.QKB.append(line)
-        print("="*87+"\n"+"Question KB is appended. Length is: "+str(len(self.QKB)))
-        self.KBlength = len(self.QKB)
-
-    def appendSubjectKB(self, SubjectFile):
-        with open(SubjectFile, 'r') as f:
-            i = 0
-            for line in f:
-                if line.rstrip() in self.SubKB.keys():
-                    self.SubKB[line.rstrip()].append(i)
-                else:
-                    self.SubKB[line.rstrip()] = [i]
-                i += 1
-        assert(i == self.KBlength)
-        print("="*87+"\n"+"Subject KB is appended. Total Subject Count is: "+str(len(self.SubKB)))
-        print("="*87+"\n"+"Subject KB is appended. Length is: "+str(i))
-
-
-    def appendSupportKB(self, SupportFile):
-        with open(SupportFile, 'r') as f:
-            print("="*87+"\n"+"File opened: "+SupportFile)
-            for line in f:
-                self.SKB.append(line)
-        print("="*87+"\n"+"Support KB is appended. Length is: "+str(len(self.SKB)))
-
-    def appendCorrectAnswerKB(self, CorrectAnswerFile):
-        with open(CorrectAnswerFile, 'r') as f:
-            print("="*87+"\n"+"File opened: "+CorrectAnswerFile)
-            for line in f:
-                self.AKB.append(line)
-        print("="*87+"\n"+"Correct Answer KB is appended. Length is: "+str(len(self.AKB)))
+    def appendSubDict(self, SubjectFile):       
+        i = 0 # index of questions
+        for line in f:
+            if line.rstrip() in self.SubDict.keys():
+                self.SubKB[line.rstrip()].append(i)
+            else:
+                self.SubKB[line.rstrip()] = [i]
+            i += 1
+        print("[QUIZBOT] Finished Subject Dictionary Construction. Total Subject Count is: "+str(len(self.SubKB)))
