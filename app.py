126a4310dadedf3cc0d1ec52cabcac1f5ae63a43
sherry <sherry.s.ruan@gmail.com>
2018-02-04 20:51:54 -0800

    testing

--- app.py
+++ app.py
@@ -120,7 +120,10 @@ def webhook():
                                 audio_url = messaging_event["message"]["attachments"][0]["payload"]["url"]
                                 final_result = speech.transcribe(audio_url)
                                 print("[QUIZBOT] PID " + str(os.getpid())+": Transcribed Text is \""+final_result+"\"")
-                                message.send_message(sender_id, final_result)
+                                if final_result != "":
+                                    message.send_message(sender_id, "You said: " + final_result)
+                                else:
+                                    message.send_message(sender_id, "Sorry, I could not recognize it :/")
 
 
                         # someone sent us a message
