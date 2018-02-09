8661ceb37ed552dc50adc51a49babfb65fb85bf3
sherry <sherry.s.ruan@gmail.com>
2018-02-08 16:35:07 -0800

    try ffmpeg

--- app.py
+++ app.py
@@ -124,12 +124,12 @@ def webhook():
                                 audio_url = messaging_event["message"]["attachments"][0]["payload"]["url"]
                                 final_result = speech.transcribe(audio_url)
                                 
-                                # print("[QUIZBOT] PID " + str(os.getpid())+": Transcribed Text is \""+final_result+"\"")
-                                # if final_result != "":
-                                #     message.send_message(sender_id, "You said: " + final_result)
-                                # else:
-                                #     message.send_message(sender_id, "Sorry, I could not recognize it :/")
-                                # chatbot.respond_to_messagetext(final_result, sender_id, qa_model, mysql)
+                                print("[QUIZBOT] PID " + str(os.getpid())+": Transcribed Text is \""+final_result+"\"")
+                                if final_result != "":
+                                    message.send_message(sender_id, "You said: " + final_result)
+                                else:
+                                    message.send_message(sender_id, "Sorry, I could not recognize it :/")
+                                chatbot.respond_to_messagetext(final_result, sender_id, qa_model, mysql)
 
 
                         # someone sent us a message
