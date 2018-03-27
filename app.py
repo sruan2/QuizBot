137ed381e9fe0fd3734382f20bc88596249012ee
Zhengneng <qiuzhengneng@gmail.com>
2018-03-26 22:46:46 -0700

    check sender id

--- app.py
+++ app.py
@@ -77,6 +77,9 @@ def webhook():
                     if messaging_event.get("optin"):  # optin confirmation
                         pass
 
+
+                    print ("="*50)
+                    print (messaging_event)    
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     
