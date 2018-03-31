5edac0980788d48309446ecd9af5e65dfd5ad29f
Zhengneng <qiuzhengneng@gmail.com>
2018-03-30 18:15:59 -0700

    removed why_2

--- message.py
+++ message.py
@@ -497,43 +497,6 @@ def send_reminder(list):
         else:
             print("[QUIZBOT] PID " + str(os.getpid())+": Sent Reminder To " + str(user_name) + " With ID " + str(recipient_id) + " At " + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
-def send_why2_quickreply(recipient_id, support_sentence):
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": support_sentence,
-            "quick_replies": [
-                {
-                    "content_type": "text",
-                    "title": "Why",
-                    "payload": "WHY"
-                },
-                {
-                    "content_type": "text",
-                    "title": "Next Question",
-                    "payload": "NEXT_QUESTION"
-                },
-                {
-                    "content_type": "text",
-                    "title":"Switch Subject",
-                    "payload":"SWITCH_SUBJECT"
-                }
-            ]
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
-
 
 def send_gotit_quickreply(recipient_id, sentence):
 
