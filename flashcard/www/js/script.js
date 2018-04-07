8ca43c3253d08306600ab9177b16b357055acd84
Bryce Tham <brycetham@gmail.com>
2018-04-07 00:48:14 -0700

    Imported Questions for QuizBot

--- flashcard/www/js/script.js
+++ flashcard/www/js/script.js
@@ -2,21 +2,8 @@ $('document').ready(
     function() {
         $('#front').show();
         $('#back').hide();
-        $index = 0
-        $json = {
-            "questions": [
-                {"front": "QuizBot", "back": "a chatbot for learning"},
-                {"front": "Apotheosis", "back": "a model of excellence"},
-                {"front": "Consummate", "back": "having supreme mastery"},
-                {"front": "Debonair", "back": "having a sophisticated charm"},
-                {"front": "Expansive", "back": "friendly and open"},
-                {"front": "Intransigent", "back": "impervious to persuasion"},
-                {"front": "Stolid", "back": "having little emotion"},
-                {"front": "Vicissitude", "back": "variation in circumstance"}
-            ]
-        }
-        $('#front').html($json.questions[$index].front);
-        $('#back').html($json.questions[$index].back);
+        $index = 0;
+        update();
     }
 );
 
@@ -31,13 +18,17 @@ function flip() {
 };
 
 function next() {
-    $index = $index < $json.questions.length - 1 ? $index + 1 : 0;
-    $('#front').html($json.questions[$index].front);
-    $('#back').html($json.questions[$index].back);
+    $index = $index < $questions.length - 1 ? $index + 1 : 0;
+    update();
 }
 
 function prev() {
-    $index = $index > 0 ? $index - 1 : $json.questions.length - 1;
-    $('#front').html($json.questions[$index].front);
-    $('#back').html($json.questions[$index].back);
+    $index = $index > 0 ? $index - 1 : $questions.length - 1;
+    update();
+}
+
+function update() {
+    // $questions are imported from questions.js
+    $('#front').html($questions[$index].question);
+    $('#back').html($questions[$index].correct_answer[0]);
 }
