f2ffba1d2326dd132eaa55f80709429f5080a3ea
sherry <sherry.s.ruan@gmail.com>
2018-05-05 11:47:40 -0700

    a simple backend for flashcard app

---
+++ flashcard_backend/flashcard_backend.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+        return 'Hello world'
+
+    if __name__ == '__main__':
+        context = ('/etc/letsencrypt/live/smartprimer.org/fullchain.pem', '/etc/letsencrypt/live/smartprimer.org/privkey.pem')
+        app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=context)
