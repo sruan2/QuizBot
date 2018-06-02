8ed24d58abde717db2069d173c92918e7001c92d
sherry <sherry.s.ruan@gmail.com>
2018-06-02 14:05:04 -0700

    fix subject

--- QAKnowledgebase.py
+++ QAKnowledgebase.py
@@ -44,4 +44,4 @@ class QAKnowlegeBase():
 
         pretty_print("Total Subject Count is: "+str(len(self.SubDict)))
         for subject in self.SubDict.keys():
-            pretty_print("{}: {}".format(subject, len(self.subKB[subject])))
+            pretty_print("{}: {}".format(subject, len(self.SubDict[subject])))
