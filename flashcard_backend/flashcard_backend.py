6e82f9e6d4d553b681d53fc9382d7adbeb90e60e
Zhengneng <qiuzhengneng@gmail.com>
2018-06-02 12:37:45 -0700

    test logging

--- flashcard_backend/flashcard_backend.py
+++ flashcard_backend/flashcard_backend.py
@@ -36,6 +36,8 @@ def webhook():
     print ("="*50)
     print (request.get_json(force=True))
     data = request.get_json(force=True)['data']
+    print ("* *" * 50)
+    print (data)
     sender_id = data['user_id']
     if not int(sender_id) in database.show_user_id_list(mysql):
         sender_firstname = data['firstname']
