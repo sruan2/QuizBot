693c9c81c87064a7c6b0f8e7b1e81ec1a9bbf5dc
sherry <sherry.s.ruan@gmail.com>
2018-06-02 13:27:27 -0700

    clean up QA Knowledge base

--- QAKnowledgebase.py
+++ QAKnowledgebase.py
@@ -1,12 +1,15 @@
+'''QA Knowledgebase for safety, gre, and science questions'''
+
 import os
 import json
 
-class ConstructQA():
+class QAKnowlegeBase():
 
     def __init__(self, jsonFile):
         print("[QUIZBOT] PID " + str(os.getpid())+": Begin to Construct QA Knowledgebase")
         data = json.load(open(jsonFile))
 
+        self.QID = [] # questin id
         self.QKB = [] # question
         self.SKB = [] # support
         self.AKB = [] # correct answer (NOTE THIS IS A LIST!)
@@ -16,21 +19,22 @@ class ConstructQA():
         self.KBlength = len(data)
 
         for entry in data:
+            self.QID.append(entry['id'])
             self.QKB.append(entry["question"])
             self.AKB.append(entry["correct_answer"])
             self.SKB.append(entry["support"])
             self.DKB.append(entry["distractor"])
             self.SubKB.append(entry["subject"])
+
         self.appendSubDict()
 
+        print("\t\t\tTotal Number of Questions: " +str(self.KBlength))
         print("[QUIZBOT] PID " + str(os.getpid())+": Finished QA Knowledgebase Construction")
-        print("[QUIZBOT] PID " + str(os.getpid())+": Total Number of Questions: " +str(self.KBlength))
 
     def appendSubDict(self):
         for i, subject in enumerate(self.SubKB):
             if subject in self.SubDict.keys():
-                self.SubDict[subject].append(i)
+                self.SubDict[subject].append(i)  # append index in list, not qid
             else:
                 self.SubDict[subject] = [i]
-        print("[QUIZBOT] PID " + str(os.getpid())+": Finished Subject Dictionary Construction")
-        print("[QUIZBOT] PID " + str(os.getpid())+": Total Subject Count is: "+str(len(self.SubDict)))
+        print("\t\t\tTotal Subject Count is: "+str(len(self.SubDict)))
