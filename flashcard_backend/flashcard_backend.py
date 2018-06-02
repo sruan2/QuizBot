281c54b9b1f37f86661ee45cb96df2be60504003
Zhengneng <qiuzhengneng@gmail.com>
2018-06-02 12:32:56 -0700

    test logging

--- flashcard_backend/flashcard_backend.py
+++ flashcard_backend/flashcard_backend.py
@@ -31,8 +31,9 @@ def verify():
 @app.route('/logdata', methods=['POST'])
 def webhook():
 
-    print (request)
-    data = request.get_json()['data']
+    print ("="*50+request)
+    print ("="*50+request.get_json(force=True))
+    data = request.get_json(force=True)['data']
     sender_id = data['user_id']
     if not int(sender_id) in database.show_user_id_list(mysql):
         sender_firstname = data['firstname']
