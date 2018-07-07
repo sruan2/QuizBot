20dc7f796ea1f13be84f1787561ba763a4672286
Liwei Jiang <ubuntu@ip-172-31-11-198.us-west-1.compute.internal>
2018-07-07 11:51:16 -0700

    image port change to OS env variable

--- message.py
+++ message.py
@@ -43,7 +43,7 @@ def send_image(recipient_id, payload, chatbot_text, image_id):
     '''
     messaging_API.send_typing_action(recipient_id)
     image_data = chatbot_text[payload]["image"][image_id]
-    image_data["image_url"].format()
+    image_data["image_url"].format(os.environ["PORT"])
     messaging_API.send_image(recipient_id, image_data)
 
 
