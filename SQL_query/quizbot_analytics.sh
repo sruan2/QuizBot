d76365d08117434c2bf9c0ae415cd5cc41638137
Liwei Jiang <ubuntu@ip-172-31-11-198.us-west-1.compute.internal>
2018-07-07 11:41:40 -0700

    updated SQL script

--- SQL_query/quizbot_analytics.sh
+++ SQL_query/quizbot_analytics.sh
@@ -3,5 +3,7 @@
 echo First Name: "$1"
 echo Last Name: "$2"
 echo Output File: quizbot_"$1"_"$2".csv
+echo Database: "$3"
 
-mysql -u root -p -e "select  a.user_id, c.user_firstname, c.user_lastname, a.qid, a.r_time as question_time, b.answer,b.score, b.r_time as answer_time from questions a, scores b, (select user_firstname, user_lastname, user_id from users where user_firstname = '$1' and user_lastname = '$2') c  where a.user_id = b.user_id and a.qid=b.qid and a.user_id = c.user_id and a.r_time <= b.r_time order by a.r_time desc, b.r_time desc" QUIZBOT_DEV | sed 's/[\t]/,/g' > quizbot_$1_$2.csv
+mysql -u root -p -e "select  concat('\"'a.user_id), c.user_firstname, c.user_lastname, a.qid, a.r_time as question_time, b.answer,b.score, concat(b.r_time,'\"') as answer_time from questions a, scores b, (select user_firstname, user_lastname, user_id from users where user_firstname = '$1' and user_lastname = '$2') c  where a.user_id = b.user_id and a.qid=b.qid and a.user_id = c.user_id and a.r_time <= b.r_time order by a.r_time desc, b.r_time desc" '$3' | sed 's/[\t]/","/g' > quizbot_$1_$2.csv
+
