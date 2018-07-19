8c5f94a4fada37a4b7eaa96310b33bc72a7cbd37
justinxu421 <justinx@stanford.edu>
2018-07-19 16:22:01 -0700

    created heatmaps for tensorflow implementation and mittens/glove corpus. Added csv generation and updated the semi supervised model

--- similarity_model/tensorflow_sentences2vec_test.py
+++ similarity_model/tensorflow_sentences2vec_test.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 import pandas as pd
 import re
-import seaborn as sns
+from sif_implementation.utils import *
 
 print('done')
 module_url = "https://tfhub.dev/google/universal-sentence-encoder/2"
@@ -34,70 +34,49 @@ print('got here')
 
 messages = [
 		# numbers
-		['5', 'five', '4','four'],
+		'5', 'five', '4','four',
 		# true/false
-		['true', 'yes', 'no', 'false'],
+		'true', 'yes', 'no', 'false',
 		#capitalization
-		['Hydrogen', 'hydrogen', 'Carbon', 'carbon'],
+		'Hydrogen', 'hydrogen', 'Carbon', 'carbon',
 		# A, B, A and B
-		['carbon', 'carbon and hydrogen', 'hydrogen and carbon'],
+		'carbon', 'carbon and hydrogen', 'hydrogen and carbon',
 		# bonds
-		['bond', 'covalent bonds'],
+		'bond', 'covalent bonds',
 		# human activity
-		['human activity', 'careless human activity'],
+		'human activity', 'careless human activity',
 		# heat
-		['heat', 'thermal energy'],
+		'heat', 'thermal energy',
 		# atom nucleus
-		['atom nucleus', 'the nucleus'],
+		'atom nucleus', 'the nucleus',
 		# nuclear
-		['nuclear', 'medulla'],
+		'nuclear', 'medulla',
 		# A and B
-		['reproduce asexually and sexually', 'reproduce sexually and asexually'],
+		'reproduce asexually and sexually', 'reproduce sexually and asexually',
 		# plural
-		['2 hour', '2hours']]
-
-# def cosine_similarity(v1, v2):
-#     similarity = (np.dot(v1, v2.T) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
-#     return similarity
-
-def plot_similarity(labels, features, rotation):
-  corr = np.inner(features, features)
-  sns.set(font_scale=1.2)
-  g = sns.heatmap(
-      corr,
-      xticklabels=labels,
-      yticklabels=labels,
-      vmin=0,
-      vmax=1,
-      cmap="YlOrRd")
-  g.set_xticklabels(labels, rotation=rotation)
-  g.set_title("Semantic Textual Similarity")
-  print(corr)
-  plt.show()
-
+		'2 hour', '2 hours']
 
 def run_and_plot(session_, input_tensor_, messages_, encoding_tensor):
   	message_embeddings_ = session_.run(
       encoding_tensor, feed_dict={input_tensor_: messages_})
-  	plot_similarity(messages_, message_embeddings_, 90)
+  	plot_similarity(messages_, message_embeddings_, 90, 'tensorflow_heatmap.png')
 
 similarity_input_placeholder = tf.placeholder(tf.string, shape=(None))
 similarity_message_encodings = embed(similarity_input_placeholder)
 
 with tf.Session() as session:
 	session.run([tf.global_variables_initializer(), tf.tables_initializer()])
-	for message in messages:
-		run_and_plot(session, similarity_input_placeholder, message,
-               similarity_message_encodings)
-		print('plotted this')
+	run_and_plot(session, similarity_input_placeholder, messages, 
+           similarity_message_encodings)
+	print('plotted this')
 
-		# message_embeddings = session.run(embed(message))
+	# message_embeddings = session.run(embed(message))
 
-		# for i, message_embedding in enumerate(np.array(message_embeddings).tolist()):
-		# 	print("Message: {}".format(message[i]))
-		# 	print("Embedding size: {}".format(len(message_embedding)))
-		# 	message_embedding_snippet = ", ".join(
-		# 		(str(x) for x in message_embedding[:3]))
-		# 	print("Embedding: [{}, ...]\n".format(message_embedding_snippet))
+	# for i, message_embedding in enumerate(np.array(message_embeddings).tolist()):
+	# 	print("Message: {}".format(message[i]))
+	# 	print("Embedding size: {}".format(len(message_embedding)))
+	# 	message_embedding_snippet = ", ".join(
+	# 		(str(x) for x in message_embedding[:3]))
+	# 	print("Embedding: [{}, ...]\n".format(message_embedding_snippet))
 
 
