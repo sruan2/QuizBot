f4c69b059eccff8da31ce67936052dbe150a1873
justinxu421 <firepower421@cs231-vm.c.pro-router-200318.internal>
2018-07-19 07:21:12 +0000

    cleaned up file names and added some documentation

--- similarity_model/mittens_model.py
+++ similarity_model/mittens_model.py
@@ -1,14 +1,20 @@
 import pickle
+import numpy as np
+
 from mittens import Mittens
 
-# store the original model to append to and fit to 
-glove_file = 'C:/Users/Justin Xu/Desktop/glove.6B.100d.pkl'
-# file = 'C:/Users/Justin Xu/Desktop/SciQ dataset-2 3/vectors.pkl'
-# file = 'C:/Users/Justin Xu/Desktop/paragram_vectors.pkl'
+''' this is a mittens model to construct new vectors
+
+2018 July 19
+'''
+
+glove_file = 'glove.6B.100d.pkl'
+# glove_file = 'vectors.pkl'
+# glove_file = 'paragram_vectors.pkl'
 
 # files containing the cooccurrence matrix and matching vocabulary
-cooccurrence_file = 'C:/Users/Justin Xu/Desktop/SciQ dataset-2 3/weighted_matrix.pkl'
-vocab_file = 'C:/Users/Justin Xu/Desktop/SciQ dataset-2 3/vocab.pkl'
+cooccurrence_file = 'weighted_matrix.pkl'
+vocab_file = 'vocab.pkl'
 
 with open(glove_file, 'rb') as pkl:
 	glove = pickle.load(pkl)
@@ -22,12 +28,19 @@ with open(vocab_file, 'rb') as pkl:
 	vocab = pickle.load(pkl)
 	print('loaded vocab')
 
-mittens_model = Mittens(n=300, max_iter=1000)
+# convert to numpy array
+cooccurrence = cooccurrence.toarray()
+
+# check if sizes match up
+print(len(vocab))
+print(cooccurrence.shape)
+
+mittens_model = Mittens(n=100, max_iter=1000)
 # Note: n must match the original embedding dimension
 new_embeddings = mittens_model.fit(
     cooccurrence,
     vocab=vocab,
-    initial_embedding_dict= glove)
+    initial_embedding_dict=glove)
 
 # transform the vectors into a dictionary mapping and then append the vocabulary to glove
 embeddings_dict = {key:np.array(vector) for key,vector in zip(cooccurrence.index, new_embeddings)}
@@ -35,7 +48,7 @@ glove.update(embeddings_dict)
 
 import _pickle as cPickle
 
-save_file = 'C:/Users/Justin Xu/Desktop/SciQ dataset-2 3/mittens_model.pkl'
+save_file = 'mittens_model.pkl'
 
 # save to mitten_model
 with open(save_file, 'wb') as output:
