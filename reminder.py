8cea8b1d578b87de17d266ef541fd70604dec7f2
Liwei Jiang <ubuntu@ip-172-31-11-198.us-west-1.compute.internal>
2018-07-20 16:28:36 -0700

    rewriting data logging (sherry) and added chat with me (liwei)

--- reminder.py
+++ reminder.py
@@ -1,22 +1,26 @@
 from threading import Timer
+from message import *
 import message
 from time import *
+from datetime import datetime
 from utils import *
 
+
 class RepeatedTimer(object):
-    def __init__(self, offset, interval, function, *args, **kwargs):
-        self.offset     = offset
-        self.interval   = interval
-        self.function   = function
-        self.args       = args
-        self.kwargs     = kwargs
+    def __init__(self, interval, function, *args, **kwargs):
+        self.offset = self.get_offset_time(20, 00, 0)
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
         self.is_running = False
-        self._timer     = None
+        self._timer = None
         self._offset_timer = None
         self.offset_time()
 
     def offset_time(self):
-        pretty_print('Reminder offsets at ' + strftime("%Y-%m-%d %H:%M:%S", localtime()), mode='Reminder')
+        pretty_print('Reminder offsets at ' +
+                     strftime("%Y-%m-%d %H:%M:%S", localtime()), mode='Reminder')
         self._offset_timer = Timer(self.offset, self.offset_run)
         self._offset_timer.start()
 
@@ -36,3 +40,50 @@ class RepeatedTimer(object):
 
     def stop(self):
         self._timer.cancel()
+
+    def get_offset_time(self, hour, minute, second):
+        FMT = "%Y-%m-%d %H:%M:%S"
+        cur_time = localtime()
+
+        _year = cur_time.tm_year
+        _mon = cur_time.tm_mon
+        _mday = cur_time.tm_mday
+        _hour = hour
+        _min = minute
+        _sec = second
+        _wday = cur_time.tm_wday
+        _yday = cur_time.tm_yday
+        _isdst = cur_time.tm_isdst
+
+        cur_time = strftime(FMT, cur_time)
+        offset_time = strftime(FMT, (_year, _mon, _mday, _hour, _min, _sec, _wday, _yday, _isdst))
+        tdelta = datetime.strptime(offset_time, FMT) - datetime.strptime(cur_time, FMT)
+
+        return tdelta.seconds
+
+
+class Reminder():
+    def __init__(self, template_conversation, mysql):
+        self.users = {}
+        self.template_conversation = template_conversation
+        self.mysql = mysql
+
+    def send_reminder(self, list):
+        '''
+            This function sends a reminder to the specified recipient.
+                Args:
+                    list: a list of user ids to which the reminder is sent
+
+                Returns:
+                    None
+        '''
+        print(self.users)
+        for recipient_id, user_name in list:
+            if recipient_id not in self.users:
+                self.users[recipient_id] = 0
+            if self.users[recipient_id] < 7:
+                self.users[recipient_id] += 1
+                send_format_quick_reply_text(
+                    recipient_id, self.mysql, self.template_conversation, "REMINDER", user_name)
+                print("[QUIZBOT] PID " + str(os.getpid())+": Sent Reminder To " + str(user_name) +
+                      " With ID " + str(recipient_id) + " At " + strftime("%Y-%m-%d %H:%M:%S", localtime()))
