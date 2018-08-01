d3849a1190c2a3d126fcc5120f14e59966738311
justinxu421 <justinx@stanford.edu>
2018-07-31 17:27:54 -0700

    documented the question sequencing models

--- question_sequencing/dash_model.py
+++ question_sequencing/dash_model.py
@@ -1,5 +1,4 @@
 '''Dash Question Sequencing Model
-
 2018 July 6
 '''
 import numpy as np
@@ -11,18 +10,26 @@ from base_model import BaseSequencingModel
 
 np.random.seed(42)
 
-# set the weights for the windows, right now based on 1/sqrt(W-w+1)
+
 def window_weights(num_windows):
+    '''Returns:
+        the weights for the windows, right now based on 1/sqrt(W-w+1)
+    '''
     weights = 1 / (np.arange(1, num_windows + 1, 1))**0.5
     return weights[::-1]
 
+
 def sigmoid(x):
+    '''sigmoid function'''
     return 1 / (1 + np.exp(-x))
 
+
 class DASHSequencingModel(BaseSequencingModel):
-    '''Pick next question using leitner sequence model'''
     def __init__(self, qa_kb, max_steps=1000, num_windows=100, threshold=0.01,
-                 student_ability=0, decay_student_ability=0, verbose = False):
+                 student_ability=0, decay_student_ability=0, verbose=False):
+        '''Sequencing model that uses DASH: Difficulty, Ability, Student History parameters
+        to find memory likelihood probabilities for each question
+        '''
         BaseSequencingModel.__init__(self, qa_kb)
         self.verbose = verbose
         self.num_items = self.QA_KB.KBlength
@@ -37,75 +44,100 @@ class DASHSequencingModel(BaseSequencingModel):
         self.curr_step = defaultdict(int)
         self.curr_item = {}
         self.update_time = {}
-        self.last_viewed = defaultdict(lambda: np.ones(self.num_items) * time.time())
-        self.num_correct = defaultdict(lambda: np.zeros((self.num_items, self.num_windows)))
-        self.num_attempts = defaultdict(lambda: np.zeros((self.num_items, self.num_windows)))
+        self.last_viewed = defaultdict(
+            lambda: np.ones(self.num_items) * time.time())
+        self.num_correct = defaultdict(
+            lambda: np.zeros((self.num_items, self.num_windows)))
+        self.num_attempts = defaultdict(
+            lambda: np.zeros((self.num_items, self.num_windows)))
 
         # parameters to be adjusted, set to default values for now
         # self.delay_coeff = np.exp(np.random.normal(0, 0.01))
         self.delay_coeff = .01
         self.student_ability = student_ability
         self.decay_student_ability = decay_student_ability
-        self.item_difficulties = np.random.normal(1,1,self.num_items) 
-        self.decay_item_difficulties = np.exp(np.random.normal(1,1,self.num_items))
+        self.item_difficulties = np.random.normal(1, 1, self.num_items)
+        self.decay_item_difficulties = np.exp(
+            np.random.normal(1, 1, self.num_items))
         window_cw = window_weights(self.num_windows)
         window_nw = window_weights(self.num_windows)
-        self.window_weights_cw = np.tile(window_cw, self.num_items).reshape((self.num_items, self.num_windows))
-        self.window_weights_nw = np.tile(window_nw, self.num_items).reshape((self.num_items, self.num_windows))
+        self.window_weights_cw = np.tile(window_cw, self.num_items).reshape(
+            (self.num_items, self.num_windows))
+        self.window_weights_nw = np.tile(window_nw, self.num_items).reshape(
+            (self.num_items, self.num_windows))
 
-
-    # current window of the current step
     def get_current_window(self, step):
+        '''Return: current window of the current step'''
         return (step % self.max_steps // self.window_size)
 
-    # study history function to account for impact on recall 
-    # sum_w (window_weight_cw * log(1+c_w) + window_weight_nw * log(1+n_w))
     def get_study_histories(self, user_id):
+        '''study history function to account for impact on recall
+        formula: sum_w (window_weight_cw * log(1+c_w) + window_weight_nw * log(1+n_w))
+        '''
         curr_window = self.get_current_window(self.curr_step[user_id])
         # do calculations up to the current window
-        return (self.window_weights_cw[:,:curr_window]*np.log(1 + self.num_correct[user_id][:,:curr_window]) +
-                self.window_weights_nw[:,:curr_window]*np.log(1 + self.num_attempts[user_id][:,:curr_window])).sum(axis = 1)
+        return (self.window_weights_cw[:, :curr_window]*np.log(1 + self.num_correct[user_id][:, :curr_window]) +
+                self.window_weights_nw[:, :curr_window]*np.log(1 + self.num_attempts[user_id][:, :curr_window])).sum(axis=1)
 
-    '''
-    formula to get likelihoods m(1+r*D)^{-f})
-    D is the time since the last review
-    r is delay coefficient
-    m = sigmoid(student_ability - item_difficulty + study_history)
-    f = exp(decay_student_ability - decay_item_difficulty)
-    '''
     def get_likelihoods(self, user_id):
+        '''
+        formula to get likelihoods m(1+r*D)^{-f})
+        D is the time since the last review
+        r is delay coefficient
+        m = sigmoid(student_ability - item_difficulty + study_history)
+        f = exp(decay_student_ability - decay_item_difficulty)
+        '''
         study_histories = self.get_study_histories(user_id)
-        m = sigmoid(self.student_ability - self.item_difficulties + study_histories)
+        m = sigmoid(self.student_ability -
+                    self.item_difficulties + study_histories)
         f = np.exp(self.decay_item_difficulties - self.decay_student_ability)
         # time difference from last viewed
         self.update_time[user_id] = time.time()
         delays = self.update_time[user_id] - self.last_viewed[user_id]
         return m * np.power(1 + self.delay_coeff * delays, -f)
 
-    # random question selection
     def pickRandomQuestion(self, user_id, subject):
+        '''Method to select a random question, for breadth selection purposes
+
+        Returns:
+            Data dictionary: {'question':  
+                              'qid' :  
+                              'correct_answer' :
+                              'support' : 
+                              'distractor' : } 
+        '''
         if subject == 'random':
             QID = random.randint(0, self.QA_KB.KBlength)
-        # if subject is not random, then pick from the respective subject question bank
         else:
+            # if subject is not random, then pick from the respective subject question bank
             QID = random.choice(self.QA_KB.SubDict[subject])
 
-        # set current item 
+        # set current item
         self.curr_item[user_id] = QID
 
-        data = {'question' : self.QA_KB.QKB[QID],
-                'qid' : QID,
+        data = {'question': self.QA_KB.QKB[QID],
+                'qid': QID,
                 'correct_answer': self.QA_KB.AKB[QID],
-                'support' : self.QA_KB.SKB[QID],
-                'distractor' : self.QA_KB.DKB[QID]}
+                'support': self.QA_KB.SKB[QID],
+                'distractor': self.QA_KB.DKB[QID]}
 
         return data
- 
-    def thresholdPickQuestion(self, user_id, subject):
-        likelihoods = self.get_likelihoods(user_id) 
 
+    def thresholdPickQuestion(self, user_id, subject):
+        '''Method to select a question given a threshold, figures out the most urgent question to review
+
+        Returns:
+            Data dictionary: {'question':  
+                              'qid' :  
+                              'correct_answer' :
+                              'support' : 
+                              'distractor' : }
+        '''
+        likelihoods = self.get_likelihoods(user_id)
+
+        # prints out the likelihoods if verbose flag on
         if self.verbose:
-            np.set_printoptions(suppress = True)
+            np.set_printoptions(suppress=True)
             print(likelihoods)
 
         # pick next item by closest to threshold
@@ -124,29 +156,44 @@ class DASHSequencingModel(BaseSequencingModel):
 
         QID = self.curr_item[user_id]
 
-        data = {'question' : self.QA_KB.QKB[QID],
-                'qid' : QID,
+        data = {'question': self.QA_KB.QKB[QID],
+                'qid': QID,
                 'correct_answer': self.QA_KB.AKB[QID],
-                'support' : self.QA_KB.SKB[QID],
-                'distractor' : self.QA_KB.DKB[QID]}
+                'support': self.QA_KB.SKB[QID],
+                'distractor': self.QA_KB.DKB[QID]}
 
         return data
 
-    # pick threshold based review every 10 steps, otherwise pick random
-    def pickNextQuestion(self, user_id = 0, subject = 'random'):
+    def pickNextQuestion(self, user_id=0, subject='random'):
+        '''pick threshold based review every 5 steps, otherwise pick random
+
+        Returns:
+            Data dictionary: {'question':  
+                              'qid' :  
+                              'correct_answer' :
+                              'support' : 
+                              'distractor' : } 
+        '''
         if self.curr_step[user_id] % 5 == 0:
             return self.thresholdPickQuestion(user_id, subject)
         else:
             return self.pickRandomQuestion(user_id, subject)
 
-    # function to update the history parameters in the model
-    def updateParameters(self, question, outcome, timestamp, user_id):
-        # get and take a step
+
+    def updateHistory(self, user_id, user_data):
+        '''outcome is either 0 or 1, if the user answered correctly
+        item is the index of the last item
+
+        Args:
+           user_data: tuples of qid(int), outcome (float [0,1]), timestamp (str) 
+        '''
+        question, outcome, timestamp = user_data
+
         step = self.curr_step[user_id]
         self.curr_step[user_id] += 1
         curr_window = self.get_current_window(step)
 
-        #convert timestamp string to seconds
+        # convert timestamp string to seconds
         time_object = time.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
         time_seconds = time.mktime(time_object)
 
@@ -154,18 +201,3 @@ class DASHSequencingModel(BaseSequencingModel):
             self.num_correct[user_id][question, curr_window] += 1
         self.num_attempts[user_id][question, curr_window] += 1
         self.last_viewed[user_id][question] = time_seconds
-
-    # outcome is either 0 or 1, if the user answered correctly 
-    # item is the index of the last item
-    def updateHistory(self, user_id, user_data):
-        qid, outcome, timestamp = user_data
-        # question = self.curr_item[user_id]
-        # time = self.update_time[user_id]
-
-        self.updateParameters(qid, outcome, timestamp, user_id)
-
-
-    # TODO: implement loading user data from file
-    def loadUserData(self, user_id, user_history_data):
-        for user_data in user_history_data:
-            self.updateHistory(user_id, user_data)
