5941badca908d67496dd4d94d715ba559fae1d4e
Liwei Jiang <ljiang@colby.edu>
2018-08-03 13:39:57 -0700

    update the flashcard cordova app to fix the reminder

--- flashcard_cordova/platforms/ios/platform_www/cordova_plugins.js
+++ flashcard_cordova/platforms/ios/platform_www/cordova_plugins.js
@@ -23,24 +23,6 @@ module.exports = [
     "clobbers": [
       "cordova.plugins.notification.local"
     ]
-  },
-  {
-    "id": "cordova-plugin-local-notification.LocalNotification.Core",
-    "file": "plugins/cordova-plugin-local-notification/www/local-notification-core.js",
-    "pluginId": "cordova-plugin-local-notification",
-    "clobbers": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
-    ]
-  },
-  {
-    "id": "cordova-plugin-local-notification.LocalNotification.Util",
-    "file": "plugins/cordova-plugin-local-notification/www/local-notification-util.js",
-    "pluginId": "cordova-plugin-local-notification",
-    "merges": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
-    ]
   }
 ];
 module.exports.metadata = 
@@ -48,7 +30,7 @@ module.exports.metadata =
 {
   "cordova-plugin-badge": "0.8.7",
   "cordova-plugin-device": "2.0.2",
-  "cordova-plugin-local-notification": "0.9.0-beta.2",
+  "cordova-plugin-local-notification": "0.9.0-beta.3",
   "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
