5fa7c3f17ec3b0bda4d956cc866abbb47364c127
Liwei Jiang <ubuntu@ip-172-31-11-198.us-west-1.compute.internal>
2018-08-22 15:32:41 -0700

    update user data

--- util/app_user_data/flashcard_user_data.py
+++ util/app_user_data/flashcard_user_data.py
@@ -19,7 +19,7 @@ result_filename = "flashcard_data_analysis.txt"
 
 # within-subject users
 users = ["Yinuo_Yao", "Dee Dee_Thao", "Jenn_Hu", "jingyi_li", "Joy_Yuzuriha", "Tyler_Yep", \
-         "Andrew_Ying", "Henry_Qin", "Nina_Horowitz"]
+         "Andrew_Ying", "Henry_Qin", "Nina_Horowitz", "Danial_Do", "Claire_Yang"]
 
 
 # 54 questions in post-study quiz (quiz B)
@@ -29,11 +29,11 @@ postquiz_qid = set([146, 145, 118, 130, 148, 117, 127, 111, 120, 141, 143, 147, 
                    9, 41, 29, 42])
 
 qualtricsID_2_qid = {0: 146, 1: 72, 2: 25, 3: 145, 4: 79, 6: 90, 7: 118, 8: 84, 9: 24, 10: 1, \
-                     11: 58, 12: 130, 13: 148, 14: 19, 15: 117, 16: 83, 17: 21, 18: 59, 19: 127, \
-                     22: 111, 23: 50, 24: 44, 25: 17, 26: 30, 27: 56, 28: 120, 29: 141, 30: 73, \
-                     31: 38, 32: 143, 33: 6, 34: 147, 35: 8, 36: 114, 37: 93, 38: 2, 39: 11, \
-                     41: 121, 42: 68, 43: 53, 44: 37, 45: 9, 46: 77, 49: 75, 50: 106, 51: 97, \
-                     52: 102, 53: 112, 54: 41, 55: 89, 56: 29, 57: 42, 58: 133, 59: 87}
+                    11: 58, 12: 130, 13: 148, 14: 19, 15: 117, 16: 83, 17: 21, 18: 59, 19: 127, \
+                    22: 111, 23: 50, 24: 44, 25: 17, 26: 30, 27: 56, 28: 120, 29: 141, 30: 73, 31: 38, \
+                    32: 143, 33: 6, 34: 147, 35: 8, 36: 114, 37: 93, 38: 2, 39: 11, 41: 121, 42: 68, \
+                    43: 53, 44: 37, 45: 9, 46: 77, 49: 75, 50: 106, 51: 97, 52: 102, 53: 112, 54: 41, \
+                    55: 89, 56: 29, 57: 42, 58: 133, 59: 87}
 
 qid_2_qualtricsID_dict = {value: key for key, value in qualtricsID_2_qid.items()}
 
@@ -125,3 +125,33 @@ f.write(output_string)
 f.close()
 print(output_string)
 
+
+
+# for user in users:
+#     flashcard_filename = os.path.join(dirname, "../../SQL_query/user_data/flashcard_" + user + ".csv")
+
+#     print(user)
+
+#     with open(flashcard_filename, 'rt') as csvfile:
+#         reader = list(csv.reader(csvfile))
+#         flashcard_file = reader[1:]
+
+#     sub_time_report = []
+#     day_counter = 0
+#     total_usage_time = 0
+
+#     old_time_stamp = flashcard_file[0][TIME_STAMP_INDEX]
+#     old_time_stamp = datetime.strptime(old_time_stamp, "%Y-%m-%d %H:%M:%S")
+
+#     for i in range(1,len(flashcard_file)):
+#         time_stamp = flashcard_file[i][TIME_STAMP_INDEX]
+#         time_stamp = datetime.strptime(time_stamp, "%Y-%m-%d %H:%M:%S")
+
+#         if (time_stamp.year, time_stamp.month, time_stamp.day) == (2018, 8, 21):
+
+#             if time_stamp.hour == 21 or time_stamp.hour == 22:
+#                 if (time_stamp - old_time_stamp).total_seconds() <= BREAK_TIME:
+#                     total_usage_time += (time_stamp - old_time_stamp).total_seconds()
+#         old_time_stamp = time_stamp
+
+#     print(total_usage_time/60)
